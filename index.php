2289a269f7132903c360f28feaa891111d4c2bc7
Guilherme Roberge Daleffe <daleffe@users.noreply.github.com>
2023-02-24 23:19:50 -0300

    Removed "exit" calls and filesystem path checks

--- index.php
+++ index.php
@@ -41,8 +41,9 @@ if (defined('ENVIRONMENT'))
 			error_reporting(0);
 		break;
 
-		default:
-			exit('The application environment is not set correctly.');
+		default:		    
+			echo('The application environment is not set correctly.');
+			return;
 	}
 }
 
@@ -122,37 +123,18 @@ if (defined('ENVIRONMENT'))
  */
 	// $assign_to_config['name_of_config_item'] = 'value of config item';
 
-
-
 // --------------------------------------------------------------------
 // END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
-// --------------------------------------------------------------------
+// --------------------------------------------------------------------	
 
-/*
- * ---------------------------------------------------------------
- *  Resolve the system path for increased reliability
- * ---------------------------------------------------------------
- */
+// Set the current directory correctly for CLI requests
+if (defined('STDIN'))
+{
+	chdir(dirname(__FILE__));
+}
 
-	// Set the current directory correctly for CLI requests
-	if (defined('STDIN'))
-	{
-		chdir(dirname(__FILE__));
-	}
-
-	if (realpath($system_path) !== FALSE)
-	{
-		$system_path = realpath($system_path).'/';
-	}
-
-	// ensure there's a trailing slash
-	$system_path = rtrim($system_path, '/').'/';
-
-	// Is the system path correct?
-	if ( ! is_dir($system_path))
-	{
-		exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
-	}
+// ensure there's a trailing slash
+$system_path = rtrim($system_path, '/').'/';
 
 /*
  * -------------------------------------------------------------------
@@ -169,27 +151,15 @@ if (defined('ENVIRONMENT'))
 	// Path to the system folder
 	define('BASEPATH', str_replace("\\", "/", $system_path));
 
+
 	// Path to the front controller (this file)
 	define('FCPATH', str_replace(SELF, '', __FILE__));
 
 	// Name of the "system folder"
 	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
 
-
 	// The path to the "application" folder
-	if (is_dir($application_folder))
-	{
-		define('APPPATH', $application_folder.'/');
-	}
-	else
-	{
-		if ( ! is_dir(BASEPATH.$application_folder.'/'))
-		{
-			exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF);
-		}
-
-		define('APPPATH', BASEPATH.$application_folder.'/');
-	}
+	define('APPPATH', $application_folder.'/');
 
 /*
  * --------------------------------------------------------------------
