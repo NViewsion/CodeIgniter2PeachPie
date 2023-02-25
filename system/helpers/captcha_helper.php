8a879b9621c889858bfc1465ed4a3518e4613d0f
Guilherme Roberge Daleffe <daleffe@users.noreply.github.com>
2023-02-25 03:22:56 -0300

    Removed MCrypt and OpenSSL functions

--- system/helpers/captcha_helper.php
+++ system/helpers/captcha_helper.php
@@ -317,15 +317,9 @@ if ( ! function_exists('create_captcha'))
 
 	function _ci_captcha_get_random_bytes($length)
 	{
-		if (defined('MCRYPT_DEV_URANDOM'))
-		{
-			return mcrypt_create_iv($length, MCRYPT_DEV_URANDOM);
+		if (function_exists('random_bytes')) {
+			return random_bytes($length);
 		}
-		elseif (function_exists('openssl_random_pseudo_bytes'))
-		{
-			return openssl_random_pseudo_bytes($length);
-		}
-
 		return FALSE;
 	}
 }
